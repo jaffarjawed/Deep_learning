8c855f604cfc74d9ee3bfdfa8be3e7c3bb0d35ef
Jaffar <jaffarjawed@gmail.com>
2021-04-13 16:05:16 +0530

    deleted 56th line

--- CNN.py
+++ CNN.py
@@ -53,7 +53,7 @@ test_set = test_datagen.flow_from_directory(
                                                         target_size=(64, 64),
                                                         batch_size=32,
                                                         class_mode='binary')
-#classifier
+
 classifier.fit_generator(
                     training_set,
                     steps_per_epoch=8000,
